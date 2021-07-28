e2aaec47b834f13f6e7cad25ba058f34a592ffda
vuqt06 <vuqt@umich.edu>
2021-07-28 09:50:23 +0700

    Initial Commit

---
+++ books-app/books_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  books_appApp.swift
+//  books-app
+//
+//  Created by Vu Trinh on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct books_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
